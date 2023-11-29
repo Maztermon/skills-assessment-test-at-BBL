430d2d722fc369ba40458d4b4f26c5fc9a5db5cc
Diamond <43300766+Maztermon@users.noreply.github.com>
2023-11-30 00:16:17 +0700

    Update App.tsx

--- Frontend-Web/src/App.tsx
+++ Frontend-Web/src/App.tsx
@@ -1,30 +1,21 @@
 import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import AppContextProvider from "./context/appContext";
-// import ProfilePage from "./pages/Profile";
 import HomePage from "./pages/Home";
 import UserPage from "./pages/Login";
 import DetailPage from "./pages/Detail";
-// import AuthContextProvider from "./context/authContext";
+
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <AuthContextProvider> */}
-      {/* <AppContextProvider> */}
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/detail/:id" element={<DetailPage />} />
           <Route path="/user/:type" element={<UserPage />} />
-          {/* <Route path="/profile" element={<ProfilePage />} />
-              
-               */}
         </Routes>
       </div>
-      {/* </AppContextProvider> */}
-      {/* </AuthContextProvider> */}
     </BrowserRouter>
   );
 }
